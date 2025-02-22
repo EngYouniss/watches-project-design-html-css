e2430219e1bf2e53941e872e1aa15259c9f6d2e9
Younis Tallan <engyounistallan@gmail.com>
2025-02-22 13:25:50 +0300

    this is the first initial watches project

---
+++ js/js.js
@@ -0,0 +1,7 @@
+
+  // يستدعي الفورم والاينبت  السبميت 
+  document.getElementById("formm").addEventListener("submit",function(event){
+ event.preventDefault();// يمنع ارسال اي شي
+var input=document.getElementById("formm").value;
+alert("thank you for communication.");
+  });
